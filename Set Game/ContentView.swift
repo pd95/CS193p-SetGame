9f88bb1c617b4e9d366f29a86de4e9f31e93fb40
Philipp <pd95@users.noreply.github.com>
2020-06-02 22:55:44 +0200

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Philipp on 02.06.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
